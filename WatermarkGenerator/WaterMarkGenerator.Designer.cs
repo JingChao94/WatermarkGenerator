f1ee96233bfd613b0c66e9d1fbb9ad718ce7e77c
VictorJing <694527037@qq.com>
2020-11-01 02:34:11 +0800

    添加项目文件。

---
+++ WatermarkGenerator/WaterMarkGenerator.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace WatermarkGenerator
+{
+    partial class WaterMarkGenerator
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WaterMarkGenerator));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.lblConstructionArea = new System.Windows.Forms.Label();
+            this.tbConstructionArea = new System.Windows.Forms.TextBox();
+            this.lblLocation = new System.Windows.Forms.Label();
+            this.tbLocation = new System.Windows.Forms.TextBox();
+            this.lblRemarks = new System.Windows.Forms.Label();
+            this.tbRemarks = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnLoadImg = new System.Windows.Forms.Button();
+            this.btnView = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pbImgView = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImgView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1030, 543);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.groupBox2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(322, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(50);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(708, 543);
+            this.panel2.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.Info;
+            this.groupBox2.Controls.Add(this.pbImgView);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Bold);
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(50);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(708, 543);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "预览区";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Controls.Add(this.tableLayoutPanel1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Bold);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(50);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(290, 543);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "操作区";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.OutsetDouble;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 25);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(284, 515);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.lblConstructionArea);
+            this.flowLayoutPanel1.Controls.Add(this.tbConstructionArea);
+            this.flowLayoutPanel1.Controls.Add(this.lblLocation);
+            this.flowLayoutPanel1.Controls.Add(this.tbLocation);
+            this.flowLayoutPanel1.Controls.Add(this.lblRemarks);
+            this.flowLayoutPanel1.Controls.Add(this.tbRemarks);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(6, 6);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(5);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(272, 348);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // lblConstructionArea
+            // 
+            this.lblConstructionArea.AutoSize = true;
+            this.lblConstructionArea.Location = new System.Drawing.Point(8, 10);
+            this.lblConstructionArea.Margin = new System.Windows.Forms.Padding(3, 5, 3, 10);
+            this.lblConstructionArea.Name = "lblConstructionArea";
+            this.lblConstructionArea.Size = new System.Drawing.Size(111, 19);
+            this.lblConstructionArea.TabIndex = 2;
+            this.lblConstructionArea.Text = "施工区域: ";
+            // 
+            // tbConstructionArea
+            // 
+            this.tbConstructionArea.Location = new System.Drawing.Point(8, 42);
+            this.tbConstructionArea.Multiline = true;
+            this.tbConstructionArea.Name = "tbConstructionArea";
+            this.tbConstructionArea.Size = new System.Drawing.Size(252, 54);
+            this.tbConstructionArea.TabIndex = 3;
+            this.tbConstructionArea.Text = "113号465外墙";
+            // 
+            // lblLocation
+            // 
+            this.lblLocation.AutoSize = true;
+            this.lblLocation.Location = new System.Drawing.Point(8, 104);
+            this.lblLocation.Margin = new System.Windows.Forms.Padding(3, 5, 3, 10);
+            this.lblLocation.Name = "lblLocation";
+            this.lblLocation.Size = new System.Drawing.Size(115, 19);
+            this.lblLocation.TabIndex = 4;
+            this.lblLocation.Text = "地    点: ";
+            // 
+            // tbLocation
+            // 
+            this.tbLocation.Location = new System.Drawing.Point(8, 136);
+            this.tbLocation.Multiline = true;
+            this.tbLocation.Name = "tbLocation";
+            this.tbLocation.Size = new System.Drawing.Size(252, 54);
+            this.tbLocation.TabIndex = 5;
+            this.tbLocation.Text = "苏州市姑苏区干将东路";
+            // 
+            // lblRemarks
+            // 
+            this.lblRemarks.AutoSize = true;
+            this.lblRemarks.Location = new System.Drawing.Point(8, 198);
+            this.lblRemarks.Margin = new System.Windows.Forms.Padding(3, 5, 3, 10);
+            this.lblRemarks.Name = "lblRemarks";
+            this.lblRemarks.Size = new System.Drawing.Size(111, 19);
+            this.lblRemarks.TabIndex = 6;
+            this.lblRemarks.Text = "备注信息: ";
+            // 
+            // tbRemarks
+            // 
+            this.tbRemarks.Location = new System.Drawing.Point(8, 230);
+            this.tbRemarks.Multiline = true;
+            this.tbRemarks.Name = "tbRemarks";
+            this.tbRemarks.Size = new System.Drawing.Size(252, 107);
+            this.tbRemarks.TabIndex = 7;
+            this.tbRemarks.Text = "防水防电";
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.btnLoadImg);
+            this.flowLayoutPanel2.Controls.Add(this.btnView);
+            this.flowLayoutPanel2.Controls.Add(this.btnSave);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(6, 363);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Padding = new System.Windows.Forms.Padding(20);
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(272, 146);
+            this.flowLayoutPanel2.TabIndex = 1;
+            // 
+            // btnLoadImg
+            // 
+            this.btnLoadImg.Location = new System.Drawing.Point(23, 23);
+            this.btnLoadImg.Margin = new System.Windows.Forms.Padding(3, 3, 10, 3);
+            this.btnLoadImg.Name = "btnLoadImg";
+            this.btnLoadImg.Size = new System.Drawing.Size(65, 95);
+            this.btnLoadImg.TabIndex = 0;
+            this.btnLoadImg.Text = "载入";
+            this.btnLoadImg.UseVisualStyleBackColor = true;
+            this.btnLoadImg.Click += new System.EventHandler(this.btnLoadImg_Click);
+            // 
+            // btnView
+            // 
+            this.btnView.Enabled = false;
+            this.btnView.Location = new System.Drawing.Point(101, 23);
+            this.btnView.Margin = new System.Windows.Forms.Padding(3, 3, 10, 3);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(65, 95);
+            this.btnView.TabIndex = 1;
+            this.btnView.Text = "预览";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(179, 23);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(65, 95);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "保存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // pbImgView
+            // 
+            this.pbImgView.BackColor = System.Drawing.SystemColors.Desktop;
+            this.pbImgView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbImgView.Location = new System.Drawing.Point(3, 25);
+            this.pbImgView.Name = "pbImgView";
+            this.pbImgView.Size = new System.Drawing.Size(702, 515);
+            this.pbImgView.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbImgView.TabIndex = 0;
+            this.pbImgView.TabStop = false;
+            // 
+            // WatermarkGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1030, 543);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "WatermarkGenerator";
+            this.Text = "水印生成器";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImgView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pbImgView;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label lblConstructionArea;
+        private System.Windows.Forms.TextBox tbConstructionArea;
+        private System.Windows.Forms.Label lblLocation;
+        private System.Windows.Forms.TextBox tbLocation;
+        private System.Windows.Forms.Label lblRemarks;
+        private System.Windows.Forms.TextBox tbRemarks;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button btnLoadImg;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ WatermarkGenerator/WaterMarkGenerator.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WatermarkGenerator
+{
+    public partial class WaterMarkGenerator : Form
+    {
+        private string strSavePath = string.Empty;
+        private Image img;
+        public WaterMarkGenerator()
+        {
+            InitializeComponent();
+            img = pbImgView.Image;
+        }
+
+        private void btnLoadImg_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                strSavePath = openFile.FileName.Replace(openFile.SafeFileName, string.Format("Mark_{0}", openFile.SafeFileName));
+                pbImgView.Load(openFile.FileName);
+                img = pbImgView.Image;
+                btnView.Enabled = true;
+                btnSave.Enabled = true;
+            }
+        }
+
+        private void btnView_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmap = new Bitmap(img);
+            Graphics g = Graphics.FromImage(bitmap);
+
+            //蓝色区域
+            Rectangle blueRect = new Rectangle(img.Width / 19, img.Height - (img.Height / 4) - (img.Height / 35), img.Width / 2, ((img.Height / 5) / 4) * 1);
+            FillRoundRectangle(g, new SolidBrush(Color.FromArgb(176, 0, 0, 255)), blueRect, (img.Width / 2) / 14, true);
+
+            g.DrawString("工程记录", new Font("黑体", blueRect.Width / 15), Brushes.White, (float)(blueRect.Width / 2.5), blueRect.Y + blueRect.Height / 4);
+
+            //白色区域
+            Rectangle whiteRect = new Rectangle(img.Width / 19, (img.Height - (img.Height / 4) - (img.Height / 35)) + ((img.Height / 5) / 4) * 1, img.Width / 2, ((img.Height / 5) / 4) * 3);
+            FillRoundRectangle(g, new SolidBrush(Color.FromArgb(176, Color.GhostWhite)), whiteRect, (img.Width / 2) / 14, false);
+
+            int coefficient = 0;
+            string viewStr = string.Format("{0}{1}{2}", lblConstructionArea.Text, tbConstructionArea.Text, Environment.NewLine);
+            viewStr = viewStr.Length > 18 ? viewStr.Insert(17, "\r\n") : viewStr;
+
+            g.DrawString(viewStr, new Font("黑体", whiteRect.Width / 21), Brushes.Black, blueRect.Width / 7, whiteRect.Y + blueRect.Height / 4);
+
+            if (viewStr.Length > 18)
+            {
+                coefficient++;
+            }
+
+            viewStr = string.Format("{0}{1}{2}", lblLocation.Text, tbLocation.Text, Environment.NewLine);
+            viewStr = viewStr.Length > 18 ? viewStr.Insert(17, "\r\n") : viewStr;
+
+            g.DrawString(viewStr, new Font("黑体", whiteRect.Width / 21), Brushes.Black, blueRect.Width / 7, whiteRect.Y + blueRect.Height / 4 + ((whiteRect.Width / 21) * (2 + coefficient)));
+
+            if (viewStr.Length > 18)
+            {
+                coefficient++;
+            }
+
+            viewStr = string.Format("{0}{1}{2}", lblRemarks.Text, tbRemarks.Text, Environment.NewLine);
+            viewStr = viewStr.Length > 18 ? viewStr.Insert(17, "\r\n") : viewStr;
+
+            g.DrawString(viewStr, new Font("黑体", whiteRect.Width / 21), Brushes.Black, blueRect.Width / 7, whiteRect.Y + blueRect.Height / 4 + ((whiteRect.Width / 21) * (4 + coefficient)));
+
+            //黄色圆点
+            g.FillEllipse(Brushes.Yellow, new Rectangle(img.Width / 13, (img.Height - (img.Height / 4) - (img.Height / 60)), (img.Width / 2) / 25, (img.Width / 2) / 25));
+            g.Dispose();
+            pbImgView.Image = bitmap;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            pbImgView.Image.Save(strSavePath);
+        }
+
+        private void FillRoundRectangle(Graphics g, Brush brush, Rectangle rect, int cornerRadius, bool radiusIsTop)
+        {
+            using (GraphicsPath path = CreateRoundedRectanglePath(rect, cornerRadius, radiusIsTop))
+            {
+                g.FillPath(brush, path);
+            }
+        }
+        private GraphicsPath CreateRoundedRectanglePath(Rectangle rect, int cornerRadius, bool radiusIsTop)
+        {
+            GraphicsPath roundedRect = new GraphicsPath();
+            if (radiusIsTop)
+            {
+                roundedRect.AddArc(rect.X, rect.Y, cornerRadius * 2, cornerRadius * 2, 180, 90);
+                roundedRect.AddLine(rect.X, rect.Y, rect.Right, rect.Y);
+            }
+            else
+            {
+                roundedRect.AddLine(rect.X + cornerRadius, rect.Y, rect.Right - cornerRadius * 2, rect.Y);
+            }
+
+            if (radiusIsTop)
+            {
+                roundedRect.AddArc(rect.X + rect.Width - cornerRadius * 2, rect.Y, cornerRadius * 2, cornerRadius * 2, 270, 90);
+                roundedRect.AddLine(rect.Right, rect.Y, rect.Right, rect.Y + rect.Height);
+            }
+            else
+            {
+                roundedRect.AddLine(rect.Right, rect.Y, rect.Right, rect.Y + rect.Height);
+            }
+
+            if (!radiusIsTop)
+            {
+                roundedRect.AddArc(rect.X + rect.Width - cornerRadius * 2, rect.Y + rect.Height - cornerRadius * 2, cornerRadius * 2, cornerRadius * 2, 0, 90);
+                roundedRect.AddLine(rect.Right, rect.Bottom, rect.X, rect.Bottom);
+            }
+            else
+            {
+                roundedRect.AddLine(rect.Right - cornerRadius * 2, rect.Bottom, rect.X + cornerRadius * 2, rect.Bottom);
+                roundedRect.AddLine(rect.X, rect.Y + cornerRadius * 3, rect.X, rect.Y + rect.Height);
+            }
+
+            if (!radiusIsTop)
+            {
+                roundedRect.AddArc(rect.X, rect.Bottom - cornerRadius * 2, cornerRadius * 2, cornerRadius * 2, 90, 90);
+                roundedRect.AddLine(rect.X, rect.Bottom, rect.X, rect.Y);
+            }
+            else
+            {
+                roundedRect.AddLine(rect.X, rect.Bottom - cornerRadius * 2, rect.X, rect.Y + cornerRadius * 2);
+            }
+            roundedRect.CloseFigure();
+            return roundedRect;
+        }
+    }
+}
